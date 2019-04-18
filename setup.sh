0dac8f6d3ee34220d103d69384efd159e9eff39c
lxdaixin128 <306111603@qq.com>
2019-04-18 20:59:34 +0800

    ff

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 # 安装必要前置
 yum -y install gcc libffi-devel openssl-devel zlib-devel autoconf gcc+ gcc-c++ libxml2 libxslt libxslt-devel
 
+
+
 # 安装SSDB
 wget --no-check-certificate https://github.com/ideawu/ssdb/archive/master.zip
 unzip master
@@ -18,6 +20,8 @@ cp /root/cloud/ssdb /etc/init.d
 sudo chkconfig --add ssdb
 sudo chkconfig ssdb on
 
+
+
 # 安装python3
 cd ..
 wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
@@ -25,6 +29,8 @@ tar -xf  Python-3.7.3.tgz
 cd Python-3.7.3
 ./configure --prefix=/usr/local/python3
 sudo make && make install
+
+
 
 # 获取proxy_pool和SSDBAdmin
 cd /usr/local
@@ -40,20 +46,3 @@ pip3 install APScheduler werkzeug Flask requests pymongo redis lxml
 
 
 
-# 修改SSDB配置文件
-rm -f /usr/local/ssdb/ssdb.conf
-cp /root/cloud/ssdb.conf /usr/local/ssdb
-
-# 配置SSDB自启
-cp /root/cloud/ssdb /etc/init.d
-sudo chkconfig --add ssdb
-sudo chkconfig ssdb on
-
-
-
-# 安装mysql (MariaDB)
-yum install -y mysql-server mysql mysql-deve #安装
-systemctl start mariadb #启动服务
-systemctl enable mariadb #设置开机启动
-
-mysql_secure_installation #初始化设置
